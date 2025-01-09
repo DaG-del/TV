b3412dff7e59211f323dc16b13b8992a20898c73
USER <USER@DESKTOP-FD0DPM8>
2025-01-09 16:52:41 +0530

    Initial commit

---
+++ TV_lisb_helper_file.m
@@ -0,0 +1,8 @@
+lf = 1.5; % m - length from center to front
+lr = 1.5; % m - length from center to rear
+tf = 1; % m - track width from center to one edge
+wr = 0.22; % m - wheel radius
+C3 = 0.52; % N/kg - coefficient for dry asphalt
+C2 = 23.99; % N/kg - coefficient for dry asphalt
+C1 = 1.28; % N/kg - coefficient for dry asphalt
+hCG = 0.6; % m - height of COG above ground
